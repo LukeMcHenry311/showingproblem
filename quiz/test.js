fb48359fa1cdcb12caae00807efbfc9fe42e5299
McHenry <lukemchenry311@gmail.com>
2021-08-05 05:08:37 -0600

    showingprob

---
+++ quiz/test.js
@@ -0,0 +1,72 @@
+
+
+var i = 0;
+var x = 0;
+var y = 0;
+var z = 0;
+
+// gryffindor === 1
+function increment1() {
+    i++;
+    console.log(i);
+};
+
+// hufflepuff === 2
+function increment2() {
+    x++;
+    console.log(x);
+}
+
+// ravenclaw === 3
+function increment3() {
+    y++;
+    console.log(y);
+}
+
+// slytherin === 4
+function increment4() {
+    z++;
+    console.log(z);
+}
+
+function blue() {
+    console.log(Math.max(i, x, y, z))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myFunction() {
+//     var link1="gryffindor.html"
+//     var link2="slytherin.html"
+//     var link3="ravenclaw.html"
+//     var link4="hufflepuff.html"
+//     var house =
+//     document.getElementById("results").value
+
+//     switch (house) {
+//         case "Gryffindor":
+//         window.location=link1
+//         break;
+//         case "Slytherin":
+//         window.location=link2
+//         break;
+//         case "Ravenclaw":
+//         window.location=link3
+//         break;
+//         case "Hufflepuff":
+//         window.location=link4
+//         break;
+//     }
+// }
